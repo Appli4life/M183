c0a89325ea8f84c69b203f35be57c80fba8d7e86
Sasa Djikic <sasa.djikic1@gmail.com>
2023-06-14 15:06:54 +0200

    UerViewModel, login added

---
+++ M183_Session/Session/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Session.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255), MinLength(5), Required]
+        public string UserName { get; set; }
+
+        [MaxLength(255), MinLength(8), Required]
+        public string UserPassword { get; set; }
+
+    }
+}
